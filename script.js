ca3166fa3f7f3d800693bfd96ce363fb1bf8e5db
tamus047-ship-it <tamus047@gmail.com>
2026-07-01 14:44:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ async function generate() {
     },
     body: JSON.stringify({
       text: text,
-      model_id: "eleven_multilingual_v2"
+      model_id: "eleven_multilingual_v2",
+      voice_settings: {
+        stability: 0.20,
+        similarity_boost: 0.75,
+        style: 0.60,
+        use_speaker_boost: true
+      }
     })
   });
 
