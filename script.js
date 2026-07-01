bd1d99d25a7ff42ea6af93991e5273e70f984411
tamus047-ship-it <tamus047@gmail.com>
2026-07-01 14:35:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+async function generate() {
+  const text = document.getElementById("text").value;
+
+  const response = await fetch("https://api.elevenlabs.io/v1/text-to-speech/CeAjv3teQ7WvtV4vyRFm", {
+    method: "POST",
+    headers: {
+      "xi-api-key": "sk_9a0bdc3b0379f23506dd96d06af2eee2b1920ee5a76b9417",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      text: text,
+      model_id: "eleven_multilingual_v2"
+    })
+  });
+
+  const audio = await response.blob();
+  document.getElementById("audio").src = URL.createObjectURL(audio);
+}
